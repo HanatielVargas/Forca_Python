ceefc1a8bee39ef2dd38d955508f27326d864997
Hanatiel Vargas <104468546+HanatielVargas@users.noreply.github.com>
2022-04-27 16:38:54 -0400

    Update main.py

--- main.py
+++ main.py
@@ -4,8 +4,6 @@ Jogo clássico da forca com letras e boneco sendo enforcado caso erre a letra e 
 Criando o jogo para aprender tkinter, web scrape e data storage com xlsx e sql. 
 '''
 
-#Importa funções que eu criei em outro arquivo python
-from functions import func 
 from tkinter import *
 from tkinter import ttk
 import pandas as pd
@@ -55,7 +53,7 @@ def trocartema(temas, escolhido, atual):
 
 
 #Preenche a lista que, inicialmente vazia, vai disponibilizar as palavras para o jogo (por enquanto)
-lista = func.preencherlista() 
+lista = preencherlista() 
 
 # Dicionário com os temas do app
 tema = {'Branco':{'Letra': '#000000', 
@@ -99,26 +97,26 @@ style.configure('BW.TFrame', background=tema[temaatual]['Fback'])
 baixo = Frame(root)
 baixo.pack(side='bottom')
 
-# Botão de 
-algo_rect = Canvas(baixo, bg=tema[temaatual]['Fundo'], width=300, height=55, relief='flat', bd=-1, border=-2)
-algo_rect.pack(side='left')
-btn = algo_rect.create_rectangle(300, 50, 0, 0, fill=tema[temaatual]['Fundo'], outline=tema[temaatual]['Fundo'])
-btn_txt = algo_rect.create_text(150, 25, text='Alguma Coisa', fill=tema[temaatual]['Letra'], font='Times 17 bold', activefill=tema[temaatual]['Fback'])
-algo_rect.tag_bind(btn_txt, '<Button-1>') # adicionar função
+# Botão de sair
+sair_rect = Canvas(baixo, bg=tema[temaatual]['Fundo'], width=300, height=55, relief='flat', bd=-1, border=-2)
+sair_rect.pack(side='left')
+sbtn = sair_rect.create_rectangle(300, 50, 0, 0, fill=tema[temaatual]['Fundo'], outline=tema[temaatual]['Fundo'])
+sbtn_txt = sair_rect.create_text(150, 25, text='Sair', fill=tema[temaatual]['Letra'], font='Times 17 bold', activefill=tema[temaatual]['Fback'])
+sair_rect.tag_bind(sbtn_txt, '<Button-1>', quit) # adicionar função
 
 # Botão de novo jogo
 novojogo_rect = Canvas(baixo, bg=tema[temaatual]['Fundo'], width=300, height=55, relief='flat', bd=-1, border=-2)
 novojogo_rect.pack(side='left')
 njbtn = novojogo_rect.create_rectangle(300, 50, 0, 0, fill=tema[temaatual]['Fundo'], outline=tema[temaatual]['Fundo'])
 njbtn_txt = novojogo_rect.create_text(150, 25, text='Novo Jogo', fill=tema[temaatual]['Letra'], font='Times 17 bold', activefill=tema[temaatual]['Fback'])
-novojogo_rect.tag_bind(njbtn_txt, '<Button-1>', func.novojogo)
+novojogo_rect.tag_bind(njbtn_txt, '<Button-1>', novojogo)
 
 # Botão de Mudar tema
 mudartema_rect = Canvas(baixo, bg=tema[temaatual]['Fundo'], width=300, height=55, relief='flat', bd=-1, border=-2)
 mudartema_rect.pack(side='left')
 mtbtn = mudartema_rect.create_rectangle(300, 50, 0, 0, fill=tema[temaatual]['Fundo'], outline=tema[temaatual]['Fundo'])
 mtbtn_txt = mudartema_rect.create_text(150, 25, text='Mudar Tema', fill=tema[temaatual]['Letra'], font='Times 17 bold', activefill=tema[temaatual]['Fback'])
-mudartema_rect.tag_bind(mtbtn_txt, '<Button-1>', func.trocartema)
+mudartema_rect.tag_bind(mtbtn_txt, '<Button-1>', trocartema)
 
 
 meio = Frame(root)
