96a4e3d2e157b82c18eed0c64db1432a2da87bf0
Hanatiel Vargas <104468546+HanatielVargas@users.noreply.github.com>
2022-04-27 16:33:10 -0400

    Update main.py

--- main.py
+++ main.py
@@ -8,6 +8,51 @@ Criando o jogo para aprender tkinter, web scrape e data storage com xlsx e sql.
 from functions import func 
 from tkinter import *
 from tkinter import ttk
+import pandas as pd
+
+
+def limparlista():
+    lista = []
+    palavras = pd.Series(lista, name='Palavras')
+    palavras.to_excel('palavras.xlsx')
+
+
+def novojogo(*args):
+    pass
+
+
+def preencherlista():
+    lista = list()
+    palavras = pd.read_excel('palavras.xlsx')
+    for c in palavras['Palavras']:
+        lista.append(str(c))
+    return lista
+
+
+def adicionarpalavra(lista, word):
+    from urllib3 import PoolManager, exceptions
+
+    try:
+        site = PoolManager().request('GET', f'https://www.dicio.com.br/{word.lower()}/')
+        if 'Ops' in str(site.data):
+            raise exceptions.SSLError
+    except exceptions.SSLError:
+        pass
+    else:
+        lista.append(word.capitalize())
+        palavras = pd.Series(lista, name='Palavras')
+        palavras.to_excel('.palavras.xlsx')
+
+
+def trocartema(temas, escolhido, atual):
+    if temas[escolhido] in temas:
+        return temas[escolhido]
+    else:
+        for c in temas:
+            if c == atual:
+                continue
+            return temas 
+
 
 #Preenche a lista que, inicialmente vazia, vai disponibilizar as palavras para o jogo (por enquanto)
 lista = func.preencherlista() 
