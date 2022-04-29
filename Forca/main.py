bac9dd67953001f155e536f49bfa78163f982c43
Hanatiel Vargas <104468546+HanatielVargas@users.noreply.github.com>
2022-04-28 23:36:21 -0400

    Update main.py

--- Forca/main.py
+++ Forca/main.py
@@ -17,6 +17,23 @@ class Model:
         self.palavra_adv = ('_ ' * len(self.palavra))[:-1]
         self.letras = list()
 
+        # Dicionário com os temas do app
+        self.tema = {'Branco':{'Letra': '#000000', 
+                               'Fundo': '#FFFFFF',
+                               'Fback': '#777777'}, 
+                     'Preto': {'Letra': '#FFFFFF', 
+                               'Fundo': '#000000',
+                               'Fback': '#777777'}, 
+                     'Roxo':  {'Letra': '#000000', 
+                               'Fundo': '#E396FF',
+                               'Fback': '#777777'},
+                     'Rosa':  {'Letra': '#361133',
+                               'Fundo': '#FF7975',
+                               'Fback': '#777777'}} 
+
+        self.temas = list(self.tema.keys())
+        self.temaatual = self.temas[0]
+
 
     def preencherlista(*args):
         lista = list()
@@ -51,64 +68,47 @@ class View(Frame):
 
         self.model = Model()
 
-        # Dicionário com os temas do app
-        self.tema = {'Branco':{'Letra': '#000000', 
-                               'Fundo': '#FFFFFF',
-                               'Fback': '#777777'}, 
-                     'Preto': {'Letra': '#FFFFFF', 
-                               'Fundo': '#000000',
-                               'Fback': '#777777'}, 
-                     'Roxo':  {'Letra': '#000000', 
-                               'Fundo': '#E396FF',
-                               'Fback': '#777777'},
-                     'Rosa':  {'Letra': '#361133',
-                               'Fundo': '#FF7975',
-                               'Fback': '#777777'}} 
-
-        self.temas = list(self.tema.keys())
-        self.temaatual = 'Roxo'
-
         # Parte de baixo da interface do jogo 
         self.baixo = Frame(root)
         self.baixo.pack(side='bottom')
-        self.baixo.configure(background=self.tema[self.temaatual]['Fundo'])
+        self.baixo.configure(background=self.model.tema[self.model.temaatual]['Fundo'])
 
         # Botão de sair
-        self.sair_rect = Canvas(self.baixo, bg=self.tema[self.temaatual]['Fundo'], width=300, height=55, relief='flat', bd=-1, border=-2)
+        self.sair_rect = Canvas(self.baixo, bg=self.model.tema[self.model.temaatual]['Fundo'], width=300, height=55, relief='flat', bd=-1, border=-2)
         self.sair_rect.pack(side='left')
-        self.sbtn = self.sair_rect.create_rectangle(300, 55, 0, 0, fill=self.tema[self.temaatual]['Fundo'], outline=self.tema[self.temaatual]['Fundo'])
-        self.sbtn_txt = self.sair_rect.create_text(150, 25, text='Sair', fill=self.tema[self.temaatual]['Letra'], font='Times 17 bold', activefill=self.tema[self.temaatual]['Fback'])
+        self.sbtn = self.sair_rect.create_rectangle(300, 55, 0, 0, fill=self.model.tema[self.model.temaatual]['Fundo'], outline=self.model.tema[self.model.temaatual]['Fundo'])
+        self.sbtn_txt = self.sair_rect.create_text(150, 25, text='Sair', fill=self.model.tema[self.model.temaatual]['Letra'], font='Times 17 bold', activefill=self.model.tema[self.model.temaatual]['Fback'])
         self.sair_rect.tag_bind(self.sbtn_txt, '<Button-1>', quit) # adicionar função
 
         # Botão de novo jogo
-        self.novojogo_rect = Canvas(self.baixo, bg=self.tema[self.temaatual]['Fundo'], width=300, height=55, relief='flat', bd=-1, border=-2)
+        self.novojogo_rect = Canvas(self.baixo, bg=self.model.tema[self.model.temaatual]['Fundo'], width=300, height=55, relief='flat', bd=-1, border=-2)
         self.novojogo_rect.pack(side='left')
-        self.njbtn = self.novojogo_rect.create_rectangle(300, 55, 0, 0, fill=self.tema[self.temaatual]['Fundo'], outline=self.tema[self.temaatual]['Fundo'])
-        self.njbtn_txt = self.novojogo_rect.create_text(150, 25, text='Novo Jogo', fill=self.tema[self.temaatual]['Letra'], font='Times 17 bold', activefill=self.tema[self.temaatual]['Fback'])
+        self.njbtn = self.novojogo_rect.create_rectangle(300, 55, 0, 0, fill=self.model.tema[self.model.temaatual]['Fundo'], outline=self.model.tema[self.model.temaatual]['Fundo'])
+        self.njbtn_txt = self.novojogo_rect.create_text(150, 25, text='Novo Jogo', fill=self.model.tema[self.model.temaatual]['Letra'], font='Times 17 bold', activefill=self.model.tema[self.model.temaatual]['Fback'])
         self.novojogo_rect.tag_bind(self.njbtn_txt, '<Button-1>', Controller.novojogo)
 
         # Botão de Mudar tema
-        self.mudartema_rect = Canvas(self.baixo, bg=self.tema[self.temaatual]['Fundo'], width=300, height=55, relief='flat', bd=-1, border=-2)
+        self.mudartema_rect = Canvas(self.baixo, bg=self.model.tema[self.model.temaatual]['Fundo'], width=300, height=55, relief='flat', bd=-1, border=-2)
         self.mudartema_rect.pack(side='left')
-        self.mtbtn = self.mudartema_rect.create_rectangle(300, 55, 0, 0, fill=self.tema[self.temaatual]['Fundo'], outline=self.tema[self.temaatual]['Fundo'])
-        self.mtbtn_txt = self.mudartema_rect.create_text(150, 25, text='Mudar Tema', fill=self.tema[self.temaatual]['Letra'], font='Times 17 bold', activefill=self.tema[self.temaatual]['Fback'])
-        self.mudartema_rect.tag_bind(self.mtbtn_txt, '<Button-1>', Controller.trocartema(self.temaatual, self.temas))
+        self.mtbtn = self.mudartema_rect.create_rectangle(300, 55, 0, 0, fill=self.model.tema[self.model.temaatual]['Fundo'], outline=self.model.tema[self.model.temaatual]['Fundo'])
+        self.mtbtn_txt = self.mudartema_rect.create_text(150, 25, text='Mudar Tema', fill=self.model.tema[self.model.temaatual]['Letra'], font='Times 17 bold', activefill=self.model.tema[self.model.temaatual]['Fback'])
+        self.mudartema_rect.tag_bind(self.mtbtn_txt, '<Button-1>', Controller.trocartema(self.model.temaatual, self.model.temas))
 
 
         # Parte do meio da interface do jogo
         self.meio = Frame(root)
         self.meio.pack(side='bottom')
-        self.meio.configure(background=self.tema[self.temaatual]['Fundo'])
+        self.meio.configure(background=self.model.tema[self.model.temaatual]['Fundo'])
 
         # Área de input
-        self.input_area = Entry(self.meio, bg=self.tema[self.temaatual]['Fundo'], fg=self.tema[self.temaatual]['Letra'], font='Times 17 bold', justify='center', selectborderwidth=1, bd=1, exportselection=0)
+        self.input_area = Entry(self.meio, bg=self.model.tema[self.model.temaatual]['Fundo'], fg=self.model.tema[self.model.temaatual]['Letra'], font='Times 17 bold', justify='center', selectborderwidth=1, bd=1, exportselection=0)
         self.input_area.pack(pady=30)
 
 
         # Parte de cima da interface do jogo
         self.cima = Frame(root)
         self.cima.pack()
-        self.cima.configure(background=self.tema[self.temaatual]['Fundo'])
+        self.cima.configure(background=self.model.tema[self.model.temaatual]['Fundo'])
 
         self.imagens()
         self.palavra()
@@ -123,10 +123,10 @@ class View(Frame):
 
     def palavra(self):
         # Área da palavra e as letras acertadas
-        self.palavra_lbl = Canvas(self.cima, bg=self.tema[self.temaatual]['Fundo'], height=600, width=300, relief='flat', bd=-1, border=-2)
+        self.palavra_lbl = Canvas(self.cima, bg=self.model.tema[self.model.temaatual]['Fundo'], height=600, width=300, relief='flat', bd=-1, border=-2)
         self.palavra_lbl.pack(side='left')
-        self.pllbl = self.palavra_lbl.create_rectangle(300, 600, 0, 0, fill=self.tema[self.temaatual]['Fundo'], outline=self.tema[self.temaatual]['Fundo'])
-        self.pllbl_txt = self.palavra_lbl.create_text(150, 300, text=self.model.palavra_adv, fill=self.tema[self.temaatual]['Letra'], font='Times 17 bold')
+        self.pllbl = self.palavra_lbl.create_rectangle(300, 600, 0, 0, fill=self.model.tema[self.model.temaatual]['Fundo'], outline=self.model.tema[self.model.temaatual]['Fundo'])
+        self.pllbl_txt = self.palavra_lbl.create_text(150, 300, text=self.model.palavra_adv, fill=self.model.tema[self.model.temaatual]['Letra'], font='Times 17 bold')
 
 
     def letrase(self):
@@ -155,6 +155,8 @@ class App(Tk):
     def __init__(self):
         super().__init__()
 
+        model = Model()
+
         view = View(self)
         view.pack()
 
@@ -163,7 +165,7 @@ class App(Tk):
         self.title('Forca')
         self.minsize(900, 500)
         self.geometry('900x500+225+150')
-        self.configure(background=view.tema[view.temaatual]['Fundo'])
+        self.configure(background=model.tema[model.temaatual]['Fundo'])
 
 
 if __name__ == '__main__':
