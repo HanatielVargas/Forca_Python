b360d71b5c74211bb6c03a478457478797436c19
Hanatiel Vargas <104468546+HanatielVargas@users.noreply.github.com>
2022-05-21 11:14:42 -0400

    Update app.py

--- Forca/app.py
+++ Forca/app.py
@@ -1,13 +1,19 @@
+'''
+Jogo Da Forca by Hanatiel Vargas
+Jogo clássico da forca com letras e boneco sendo enforcado caso erre a letra e a palavra.
+Criando o jogo para aprender tkinter, web scrape e data storage com xlsx e sql. 
+'''
+
 from kivy.app import App
 from kivy.lang import Builder
 import pandas as pd
 
 
-def preencherlista(self, *args):
-    self.lista = list()
-    self.palavras = pd.read_excel('Forca/database/palavras.xlsx')
-    for c in self.palavras['Palavras']:
-        self.lista.append(str(c))
+def preencherlista(*args):
+    lista = list()
+    palavras = pd.read_excel('Forca/database/palavras.xlsx')
+    for c in palavras['Palavras']:
+        lista.append(str(c))
 
 
 def adicionarpalavra(lista, word):
@@ -41,11 +47,13 @@ tema = {'Branco':{'Letra': '#000000',
                   'Fundo': '#F0F0F0',
                   'Fback': '#E396FF'}} 
 
-lista = preencherlista()
+lista_palavras = preencherlista()
 
 
 class Forca(App):
     def build(self):
+        self.icon = 'Forca/images/favicon.ico'
+        self.title = 'Jogo da Forca'
         return Builder.load_file('Forca/forca.kv')
 
 
