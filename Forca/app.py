16f202119e2215c0ad28b8e797712be2c430539e
Hanatiel Vargas <104468546+HanatielVargas@users.noreply.github.com>
2022-05-28 14:50:32 -0400

    Adicionando a base para o funcionamento

--- Forca/app.py
+++ Forca/app.py
@@ -1,11 +1,12 @@
 '''
 Jogo Da Forca by Hanatiel Vargas
 Jogo clássico da forca com letras e boneco sendo enforcado caso erre a letra e a palavra.
-Criando o jogo para aprender tkinter, web scrape e data storage com xlsx e sql. 
+Criando o jogo para aprender kivy, web scrape e data storage com xlsx e sql. 
 '''
 
 from kivy.app import App
-from kivy.lang import Builder
+from kivy.uix.screenmanager import Screen, ScreenManager, NoTransition
+from random import choice
 import pandas as pd
 
 
@@ -14,6 +15,8 @@ def preencherlista(*args):
     palavras = pd.read_excel('Forca/database/palavras.xlsx')
     for c in palavras['Palavras']:
         lista.append(str(c))
+    print(lista)
+    return lista
 
 
 def adicionarpalavra(lista, word):
@@ -31,30 +34,44 @@ def adicionarpalavra(lista, word):
         palavras.to_excel('Forca/database/palavras.xlsx')
 
 
-tema = {'Branco':{'Letra': '#000000', 
-                  'Fundo': '#FFFFFF',
-                  'Fback': '#777777'}, 
-        'Preto': {'Letra': '#FFFFFF', 
-                  'Fundo': '#000000',
-                  'Fback': '#777777'}, 
-        'Roxo':  {'Letra': '#000000', 
-                  'Fundo': '#E396FF',
-                  'Fback': '#777777'},
-        'Rosa':  {'Letra': '#361133',
-                  'Fundo': '#FF7975',
-                  'Fback': '#777777'},
-        'padrao':{'Letra': '#000000',
-                  'Fundo': '#F0F0F0',
-                  'Fback': '#E396FF'}} 
+TEMAS = {'Branco':{'Letra': '#000000', 
+                   'Fundo': '#FFFFFF',
+                   'Fback': '#777777'}, 
+         'Preto': {'Letra': '#FFFFFF', 
+                   'Fundo': '#000000',
+                   'Fback': '#777777'}, 
+         'Roxo':  {'Letra': '#000000', 
+                   'Fundo': '#E396FF',
+                   'Fback': '#777777'},
+         'Rosa':  {'Letra': '#361133',
+                   'Fundo': '#FF7975',
+                   'Fback': '#777777'},
+         'padrao':{'Letra': '#000000',
+                   'Fundo': '#F0F0F0',  
+                   'Fback': '#E396FF'}} 
+LISTA_TEMAS = list(TEMAS.keys())
+LISTA_PALAVRAS = preencherlista()
+TENTATIVAS = ''
 
-lista_palavras = preencherlista()
+palavra = choice(LISTA_PALAVRAS)
+tema_atual = LISTA_TEMAS[-1]
+imagem_atual = 'Forca/images/forca_6vidas.png'
+palavra_adv = ('_ ' * len(palavra))[:-1]
+
+    
+class Main(Screen):
+    def refresh(self):
+        self.ids.output.text = palavra_adv
+        self.ids.wrongs.text = 'erros'
 
 
 class Forca(App):
     def build(self):
         self.icon = 'Forca/images/favicon.ico'
         self.title = 'Jogo da Forca'
-        return Builder.load_file('Forca/forca.kv')
+        self.jogo_da_forca = ScreenManager(transition=NoTransition(duration=0))
+        self.jogo_da_forca.add_widget(Main(name='main'))
+        return self.jogo_da_forca
 
 
 if __name__ == '__main__':
