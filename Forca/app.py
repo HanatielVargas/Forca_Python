c69d5baf5a4ca26cd38eb363e2ddc8d87b2d3dce
Hanatiel Vargas <104468546+HanatielVargas@users.noreply.github.com>
2022-06-03 21:21:10 -0400

    Atualizando

--- Forca/app.py
+++ Forca/app.py
@@ -12,7 +12,7 @@ import pandas as pd
 
 class Main(Screen):
 
-    TEMAS = {'Branco':{'Letra': (0, 0, 0, 1), 
+    temas = {'Branco':{'Letra': (0, 0, 0, 1), 
                        'Fundo': (255, 255, 255, 1),
                        'Fback': (119, 119, 119, 1)}, 
              'Preto': {'Letra': (255, 255, 255, 1), 
@@ -27,16 +27,17 @@ class Main(Screen):
              'padrao':{'Letra': (0, 0, 0, 1),
                        'Fundo': (240, 240, 240, 1),  
                        'Fback': (227, 150, 255, 1)}} 
-    LISTA_TEMAS = list(TEMAS.keys())
-    LISTA_PALAVRAS = list()
-    TENTATIVAS = ''
+    lista_temas = list(temas.keys())
+    lista_palavras = list()
+    tentativas = ''
 
     palavras = pd.read_excel('Forca/database/palavras.xlsx')
     for c in palavras['Palavras']:
-        LISTA_PALAVRAS.append(str(c))
+        if c not in lista_palavras and c != '':
+            lista_palavras.append(str(c))
 
-    palavra = choice(LISTA_PALAVRAS)
-    tema_atual = LISTA_TEMAS[-1]
+    palavra = choice(lista_palavras)
+    tema_atual = lista_temas[-1]
     imagem_atual = 'Forca/images/forca_6vidas.png'
     palavra_adv = ('_ ' * len(palavra))[:-1]
 
@@ -51,31 +52,31 @@ class Main(Screen):
 
 
     def mudar_tema(self):
-        atual = self.LISTA_TEMAS.index(self.tema_atual)
-        tamanho = len(self.LISTA_TEMAS)
+        atual = self.lista_temas.index(self.tema_atual)
+        tamanho = len(self.lista_temas)
         print(atual)
         if atual+1 != tamanho:
-            self.tema_atual = self.LISTA_TEMAS[atual+1]
+            self.tema_atual = self.lista_temas[atual+1]
         else:
-            self.tema_atual = self.LISTA_TEMAS[0]
+            self.tema_atual = self.lista_temas[0]
 
 
     def adicionar_palavra(self):
-        global LISTA_PALAVRAS
+        
         from urllib3 import PoolManager, exceptions
 
         word = self.ids.input.text
 
         try:
             site = PoolManager().request('GET', f'https://www.dicio.com.br/{word.lower()}/')
-            if 'Ops' in str(site.data) or word == '':
+            if 'Ops' in str(site.data) or word == '' or word.capitalize() in self.lista_palavras:
                 raise exceptions.SSLError
         except exceptions.SSLError:
             pass
         else:
-            LISTA_PALAVRAS.append(word.capitalize())
-            LISTA_PALAVRAS.sort()
-            palavras = pd.Series(LISTA_PALAVRAS, name='Palavras')
+            self.lista_palavras.append(word.capitalize())
+            self.lista_palavras.sort()
+            palavras = pd.Series(self.lista_palavras, name='Palavras')
             palavras.to_excel('Forca/database/palavras.xlsx')
 
 
@@ -91,19 +92,3 @@ class Forca(App):
 
 if __name__ == '__main__':
     Forca().run()
-
-'''
-def adicionarpalavra(lista, word):
-    from urllib3 import PoolManager, exceptions
-
-    try:
-        site = PoolManager().request('GET', f'https://www.dicio.com.br/{word.lower()}/')
-        if 'Ops' in str(site.data):
-            raise exceptions.SSLError
-    except exceptions.SSLError:
-        pass
-    else:
-        lista.append(word.capitalize())
-        palavras = pd.Series(lista, name='Palavras')
-        palavras.to_excel('Forca/database/palavras.xlsx')
-'''
